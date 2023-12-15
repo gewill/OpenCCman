0e29f1612d47faa2312fc9aa1e61504c81d3196a
Ge Will <531sunlight@gmail.com>
2023-12-15 21:49:40 +0800

    Init xcodeporj

---
+++ OpenCCman/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenCCman
+//
+//  Created by will on 2023/12/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
---
+++ OpenCCman/OpenCCmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenCCmanApp.swift
+//  OpenCCman
+//
+//  Created by will on 2023/12/15.
+//
+
+import SwiftUI
+
+@main
+struct OpenCCmanApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
