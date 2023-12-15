1a07f079f2a17c1bb567d7ff800399eafc89c147
Ge Will <531sunlight@gmail.com>
2023-12-15 22:05:50 +0800

    Feat convert page

--- OpenCCman/ContentView.swift
+++ OpenCCman/ContentView.swift
@@ -5,15 +5,26 @@
 //  Created by will on 2023/12/15.
 //
 
+import OpenCC
 import SwiftUI
 
 struct ContentView: View {
+  @State var text: String = ""
+  @State var result: String = ""
+
+  let converter = try! ChineseConverter(options: [.traditionalize, .twStandard, .twIdiom])
+
   var body: some View {
     VStack {
-      Image(systemName: "globe")
-        .imageScale(.large)
-        .foregroundStyle(.tint)
-      Text("Hello, world!")
+      Text("OpenCCman").font(.title)
+      TextEditor(text: $text)
+        .frame(minHeight: 200)
+      Divider()
+      Text(result)
+      Spacer()
+      Button("Convert") {
+        result = converter.convert(text)
+      }
     }
     .padding()
   }
