36f44c272172077e32c831808503b541e73203ac
Ge Will <531sunlight@gmail.com>
2023-12-16 14:43:14 +0800

    Update layout

--- OpenCCman/ContentView.swift
+++ OpenCCman/ContentView.swift
@@ -9,18 +9,35 @@ import OpenCC
 import SwiftUI
 
 struct ContentView: View {
-  @State var text: String = ""
+  @State var text: String = "鼠标里面的硅二极管坏了，导致光标分辨率降低。"
   @State var result: String = ""
 
   let converter = try! ChineseConverter(options: [.traditionalize, .twStandard, .twIdiom])
 
   var body: some View {
-    VStack {
-      Text("OpenCCman").font(.title)
+    VStack(alignment: .leading) {
+      Group {
+        Text("OpenCCman").font(.title)
+        Text("Convert Chinese text with OpenCC")
+      }
+      .frame(maxWidth: .infinity)
+      
+      Text("Input").font(.headline)
       TextEditor(text: $text)
-        .frame(minHeight: 200)
-      Divider()
+        .frame(maxWidth: .infinity, minHeight: 200, maxHeight: 300)
+        .padding(6)
+        .background(
+          RoundedRectangle(cornerRadius: 10)
+            .stroke(Color.primary.opacity(0.3), lineWidth: 1)
+        )
+      Text("Result").font(.headline)
       Text(result)
+        .frame(maxWidth: .infinity, minHeight: 200, maxHeight: 300, alignment: .topLeading)
+        .padding(6)
+        .background(
+          RoundedRectangle(cornerRadius: 10)
+            .stroke(Color.primary.opacity(0.3), lineWidth: 1)
+        )
       Spacer()
       Button("Convert") {
         result = converter.convert(text)
