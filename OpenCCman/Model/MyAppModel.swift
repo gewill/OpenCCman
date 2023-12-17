624c20d95c72cc7de00d26a08306d81659d33ce5
Ge Will <531sunlight@gmail.com>
2023-12-17 20:42:50 +0800

    Modify my apps

--- OpenCCman/Model/MyAppModel.swift
+++ OpenCCman/Model/MyAppModel.swift
@@ -11,6 +11,6 @@ struct MyAppModel: Codable, Identifiable {
 
 let allMyApps: [MyAppModel] = [
   MyAppModel(name: "iPerfman", des: "An iPerf3 tool", link: "https://apps.apple.com/app/id6447375831", iconName: "Icon-iPerfman"),
-  MyAppModel(name: "VoiceAI Chat", des: "chat to AI with voice", link: "https://apps.apple.com/app/id6445994863", iconName: "Icon-VoiceAI Chat"),
+  MyAppModel(name: "Clickman", des: "Auto clicker for Mac", link: "https://apps.apple.com/app/id6449612559", iconName: "Icon-Clickman"),
   MyAppModel(name: "Secret Diary", des: "password notes", link: "https://apps.apple.com/app/id6445909382", iconName: "Icon-Secret Diary"),
 ]
