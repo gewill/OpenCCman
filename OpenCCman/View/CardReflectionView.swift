c332f68056d395aeedb9bff8bb7b2a6879d4c8e6
Ge Will <531sunlight@gmail.com>
2023-12-19 23:30:00 +0800

    Modify Card view to ColorfulX

--- OpenCCman/View/CardReflectionView.swift
+++ OpenCCman/View/CardReflectionView.swift
@@ -1,8 +1,10 @@
+import ColorfulX
 import SwiftUI
 
 struct CardReflectionView<Content: View>: View {
   @State var translation: CGSize = .zero
   @State var isDragging = false
+  @State var colors: [Color] = ColorfulPreset.neon.colors
   let content: Content
 
   // MARK: - life cycle
@@ -27,9 +29,7 @@ struct CardReflectionView<Content: View>: View {
 
   var body: some View {
     ZStack {
-      Image("Background 1")
-        .resizable()
-        .aspectRatio(contentMode: .fill)
+      ColorfulView(color: $colors)
         .frame(height: 200)
         .frame(maxWidth: Constant.maxiPhoneScreenWidth)
         .overlay(
@@ -37,24 +37,7 @@ struct CardReflectionView<Content: View>: View {
             content
           }
         )
-        .overlay(LinearGradient(colors: [.clear, .white.opacity(0.5), .clear], startPoint: .topLeading, endPoint: UnitPoint(x: abs(translation.height)/100+1, y: abs(translation.height)/100+1)))
-        .overlay(
-          RoundedRectangle(cornerRadius: 20)
-            .strokeBorder(.linearGradient(colors: [.clear, .white.opacity(0.75), .clear, .white.opacity(0.75), .clear], startPoint: .topLeading, endPoint: UnitPoint(x: abs(translation.width)/100+0.5, y: abs(translation.height)/100+0.5)))
-        )
-        .overlay(
-          LinearGradient(colors: [Color(#colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 0.5152369619)), Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 0.5))], startPoint: .topLeading, endPoint: .bottomTrailing)
-            .blendMode(.overlay)
-        )
         .cornerRadius(20)
-        .background(
-          RoundedRectangle(cornerRadius: 20, style: .continuous)
-            .fill(.black)
-            .offset(y: 50)
-            .blur(radius: 50)
-            .opacity(0.5)
-            .blendMode(.overlay)
-        )
         .scaleEffect(0.9)
         .rotation3DEffect(.degrees(isDragging ? 10 : 0), axis: (x: -translation.height, y: translation.width, z: 0))
         .gesture(drag)
