079f4f69511896941fcdccfe3d4edda190f01dbc
Ge Will <531sunlight@gmail.com>
2026-09-15 18:58:05 +0800

    docs: normalize the reproduction package identity

--- docs/performance/2026-09-15-contiguous-input/Package.swift
+++ docs/performance/2026-09-15-contiguous-input/Package.swift
@@ -4,5 +4,5 @@ let package = Package(
     name: "ContiguousInputBenchmark",
     platforms: [.macOS(.v11)],
     dependencies: [.package(url: "https://github.com/gewill/SwiftyOpenCC", revision: "6eded293f5c84c064f332cbc2832391165c82dda")],
-    targets: [.executableTarget(name: "ContiguousInputBenchmark", dependencies: [.product(name: "OpenCC", package: "SwiftyOpenCC")])]
+    targets: [.executableTarget(name: "ContiguousInputBenchmark", dependencies: [.product(name: "OpenCC", package: "swiftyopencc")])]
 )
